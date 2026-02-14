425ebc1d84f909b5004030988119b30f24e5d57e
sofytorresuu-hash <sofytorresu.u@gmail.com>
2026-02-13 19:10:54 -0500

    Add files via upload

---
+++ Rock Paper Scissors Game.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 20:51:47 2026
+
+@author: sofyt
+"""
+#Instrucciones Principales 
+Rock = 1
+Paper= 2
+Scissors = 3
+1>3
+2>1
+3>2
+Message = "Welcome to the Rock, Paper, Scissors Game! You will choose Rock using 1, Paper using 2 or Rock using 3. The system will make its choice and compare it. Good Luck!"
+print( Message )
+#Toma de Datos indicado en diagramas de flujo
+name = input("Write your surname: ")
+print ("Hello %s! Let's begin!" % name)
+
+first= input("Your choice is rock? (If yes write 1)")
+print (first)
+import random
+aleatory.number=random.randit (1,3)
+print(aleatory.number)
+if first==1 and aleatory.number==1:
+    print("It's a Draw!")
+elif 
